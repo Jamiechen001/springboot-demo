ef346606061a508116577b3ef5a18608ab8cacdb
chenpan <chenpanisme@163.com>
2021-06-22 14:28:55 +0800

    init

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 陈攀
+ * @date 2021/6/22 14:11
+ * @Description
+ */
+@Controller
+@RequestMapping("/app")
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String testDemo() {
+        return "Hello World!";
+    }
+}
